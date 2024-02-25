195e40f1bfe6393df915ee3d6a83e103dc46ca40
Sara Johnsson <sara86persson@gmail.com>
2024-02-25 12:00:17 +0100

    installerat vite

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HeaderComponent from "../components/header/HeaderComponent";
 import FooterComponent from "../components/header/FooterComponent";
 import MainComponent from "../views/main/MainComponent";
